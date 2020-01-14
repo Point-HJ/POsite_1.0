5114f2d602702bb755323eeee704d4623e31bd0e
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2020-01-14 15:08:36 +0200

    Add project files.

---
+++ POsite_1.0/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace POsite_1._0.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ POsite_1.0/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace POsite_1._0.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
